47f7568f01306914a654e815dc2040e4b138100d
PIDarifwidagdo <arif@panorama.id>
2023-04-21 21:08:57 +0700

    Draft

--- src/components/DateTimeDisplay.jsx
+++ src/components/DateTimeDisplay.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const DateTimeDisplay = ({ isFlip, value, type, isDanger }) => {
+const DateTimeDisplay = ({ value, isFlip }) => {
+  const [topHalf, setTopHalf] = useState(null);
+  const [bottomHalf, setBottomHalf] = useState(null);
+  const [topFlip, setTopFlip] = useState(null);
+  const [bottomFlip, setBottomFlip] = useState(null);
+
+  const flip = () => {
+    setTopHalf(document.querySelector(".top"));
+
+    const topFlip = document.createElement("div");
+    topFlip.classList.add("top-flip");
+
+    const bottomFlip = document.createElement("div");
+    bottomFlip.classList.add("bottom-flip");
+
+    document.querySelector(".bottom").append(topFlip, bottomFlip);
+  };
+  console.log(value);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (topHalf !== value) {
+        flip();
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className={`flip-card  `} data-hours-tens>
-      {/* <div className="bottom-flip"> */}
-      <div className="top">{value}</div>
-      <div className={`bottom ${isFlip}`}>{value}</div>
+    <div className={`flip-card`}>
+      <div className="">
+        <div className="top">{value}</div>
+        <div className={`bottom`}>{value}</div>
+      </div>
       {/* </div> */}
     </div>
   );
