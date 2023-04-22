1731c33a0538551d244ae9049d63f78e7dbfd320
PIDarifwidagdo <arif@panorama.id>
2023-04-22 21:13:25 +0700

    Update Display Countdown Timer

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useCountdown } from "../hooks/useCountdown";
+import DateTimeDisplay from "./DateTimeDisplay";
 
 const ExpiredNotice = () => {
   return (
@@ -11,155 +12,121 @@ const ExpiredNotice = () => {
 };
 
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
-  // const [dataDaysTens, setDataDaysTens] = useState(0);
-  // const [dataDaysOne, setDataDaysOne] = useState(0);
-  // const [dataHoursTens, setDataHoursTens] = useState(0);
-  // const [dataHoursOne, setDataHoursOne] = useState(0);
-  // const [dataMinutesTens, setDataMinutesTens] = useState(0);
-  // const [dataMinutesOne, setDataMinutesOne] = useState(0);
-  // const [dataSecondsTens, setDataSecondsTens] = useState(0);
-  // const [dataSecondsOne, setDataSecondsOne] = useState(0);
+  // function getTimeSegmentElements(segmentElement) {
+  //   const segmentDisplay = segmentElement.querySelector(".segment-display");
+  //   const segmentDisplayTop = segmentDisplay.querySelector(
+  //     ".segment-display__top"
+  //   );
+  //   const segmentDisplayBottom = segmentDisplay.querySelector(
+  //     ".segment-display__bottom"
+  //   );
 
-  const flip = (flipCard, newNumber) => {
-    const topHalf = flipCard.querySelector(".top");
-    const startNumber = parseInt(topHalf.textContent);
-    if (newNumber === startNumber) return;
+  //   const segmentOverlay = segmentDisplay.querySelector(".segment-overlay");
+  //   const segmentOverlayTop = segmentOverlay.querySelector(
+  //     ".segment-overlay__top"
+  //   );
+  //   const segmentOverlayBottom = segmentOverlay.querySelector(
+  //     ".segment-overlay__bottom"
+  //   );
 
-    const bottomHalf = flipCard.querySelector(".bottom");
-    const topFlip = document.createElement("div");
-    topFlip.classList.add("top-flip");
-    const bottomFlip = document.createElement("div");
-    bottomFlip.classList.add("bottom-flip");
+  //   return {
+  //     segmentDisplayTop,
+  //     segmentDisplayBottom,
+  //     segmentOverlay,
+  //     segmentOverlayTop,
+  //     segmentOverlayBottom,
+  //   };
+  // }
 
-    topHalf.textContent = startNumber;
-    bottomHalf.textContent = startNumber;
-    topFlip.textContent = startNumber;
-    bottomFlip.textContent = newNumber;
+  // function updateSegmentValues(displayElement, overlayElement, value) {
+  //   displayElement.textContent = value;
+  //   overlayElement.textContent = value;
+  // }
 
-    topFlip.addEventListener("animationstart", (e) => {
-      topHalf.textContent = newNumber;
-    });
-    topFlip.addEventListener("animationend", (e) => {
-      topFlip.remove();
-    });
-    bottomFlip.addEventListener("animationend", (e) => {
-      bottomHalf.textContent = newNumber;
-      bottomFlip.remove();
-    });
-    flipCard.append(topFlip, bottomFlip);
-  };
+  // function updateTimeSegment(segmentElement, timeValue) {
+  //   const segmentElements = getTimeSegmentElements(segmentElement);
 
-  useEffect(() => {
-    flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10));
-    flip(document.querySelector("[data-days-ones]"), days % 10);
-    flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
-    flip(document.querySelector("[data-hours-ones]"), hours % 10);
-    flip(
-      document.querySelector("[data-minutes-tens]"),
-      Math.floor(minutes / 10)
-    );
-    flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
-    flip(
-      document.querySelector("[data-seconds-tens]"),
-      Math.floor(seconds / 10)
-    );
-    flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
-  });
+  //   if (
+  //     parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue
+  //   ) {
+  //     return;
+  //   }
+
+  //   segmentElements.segmentOverlay.classList.add("flip");
+
+  //   updateSegmentValues(
+  //     segmentElements.segmentDisplayTop,
+  //     segmentElements.segmentOverlayBottom,
+  //     timeValue
+  //   );
+
+  //   function finishAnimation() {
+  //     segmentElements.segmentOverlay.classList.remove("flip");
+  //     updateSegmentValues(
+  //       segmentElements.segmentDisplayBottom,
+  //       segmentElements.segmentOverlayTop,
+  //       timeValue
+  //     );
+
+  //     this.removeEventListener("animationend", finishAnimation);
+  //   }
+
+  //   segmentElements.segmentOverlay.addEventListener(
+  //     "animationend",
+  //     finishAnimation
+  //   );
+  // }
+
+  // function updateTimeSection(sectionID, timeValue) {
+  //   const firstNumber = Math.floor(timeValue / 10) || 0;
+  //   console.log(firstNumber);
+  //   const secondNumber = timeValue % 10 || 0;
+  //   const sectionElement = document.getElementById(sectionID);
+  //   const timeSegments = sectionElement.querySelectorAll(".time-segment");
+
+  //   updateTimeSegment(timeSegments[0], firstNumber);
+  //   updateTimeSegment(timeSegments[1], secondNumber);
+  // }
+
+  // updateTimeSection("seconds", seconds);
+  // updateTimeSection("minutes", minutes);
+  // updateTimeSection("hours", hours);
 
   return (
-    <div className="show-counter w-full h-screen items-center justify-center flex gap-x-3">
-      <div className="container-segment">
-        <div className="segment-title">Days</div>
-        <div className="segment">
-          <div className="flip-card" data-days-tens>
-            <div className="top">2</div>
-            <div className="bottom">2</div>
-          </div>
-          <div className="flip-card" data-days-ones>
-            <div className="top">4</div>
-            <div className="bottom">4</div>
-          </div>
-        </div>
-      </div>
-      <div className="container-segment">
-        <div className="segment-title">Hours</div>
-        <div className="segment">
-          <div className="flip-card" data-hours-tens>
-            <div className="top">2</div>
-            <div className="bottom">2</div>
-          </div>
-          <div className="flip-card" data-hours-ones>
-            <div className="top">4</div>
-            <div className="bottom">4</div>
-          </div>
-        </div>
-      </div>
-      <div className="container-segment">
-        <div className="segment-title">Minutes</div>
-        <div className="segment">
-          <div className="flip-card" data-minutes-tens>
-            <div className="top">0</div>
-            <div className="bottom">0</div>
-          </div>
-          <div className="flip-card" data-minutes-ones>
-            <div className="top">0</div>
-            <div className="bottom">0</div>
-          </div>
-        </div>
-      </div>
-      <div className="container-segment">
-        <div className="segment-title">Seconds</div>
-        <div className="segment">
-          <div className="flip-card" data-seconds-tens>
-            <div className="top">0</div>
-            <div className="bottom">0</div>
-          </div>
-          <div className="flip-card" data-seconds-ones>
-            <div className="top">0</div>
-            <div className="bottom">0</div>
-          </div>
-        </div>
-      </div>
-      {/* <div>
-        <div className="text-sm">Days</div>
-        <div className="segment">
-          <DateTimeDisplay value={dataDaysTens} />
-          <DateTimeDisplay value={dataDaysOne} />
-        </div>
-      </div>
-      <div>
-        <div className="text-sm">Hours</div>
-        <div className="segment">
-          <DateTimeDisplay value={dataHoursTens} />
-          <DateTimeDisplay value={dataHoursOne} />
-        </div>
-      </div>
-      <div>
-        <div className="text-sm">Minutes</div>
-        <div className="segment">
-          <DateTimeDisplay value={dataMinutesTens} />
-          <DateTimeDisplay value={dataMinutesOne} />
-        </div>
-      </div> */}
-      {/* <div>
-        <div className="text-sm">Seconds</div>
-        <div className="segment">
-          <DateTimeDisplay value={dataSecondsTens} />
-          <DateTimeDisplay value={dataSecondsOne} />
-        </div>
-      </div> */}
+    <div className="countdown">
+      <DateTimeDisplay
+        id={"days"}
+        title={"Days"}
+        dateTime={{ days, hours, minutes, seconds }}
+      />
+      <DateTimeDisplay
+        id={"hours"}
+        title={"Hours"}
+        dateTime={{ days, hours, minutes, seconds }}
+      />
+      <DateTimeDisplay
+        id={"minutes"}
+        title={"Minutes"}
+        dateTime={{ days, hours, minutes, seconds }}
+      />
+      <DateTimeDisplay
+        id={"seconds"}
+        title={"Seconds"}
+        dateTime={{ days, hours, minutes, seconds }}
+      />
     </div>
   );
 };
 
 const CountdownTimer = ({ targetDate }) => {
-  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  const [complete, days, hours, minutes, seconds] = useCountdown(targetDate);
 
   if (days + hours + minutes + seconds <= 0) {
     return <ExpiredNotice />;
   } else {
     return (
       <ShowCounter
+        complete={complete}
         days={days}
         hours={hours}
         minutes={minutes}
--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -20,12 +20,13 @@ const useCountdown = (targetDate) => {
 
 const getReturnValues = (countDown) => {
   // calculate time left
+  const complete = Date.now() <= countDown;
   const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
   const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
 
-  return [days, hours, minutes, seconds];
+  return [complete, days, hours, minutes, seconds];
 };
 
 export { useCountdown };
